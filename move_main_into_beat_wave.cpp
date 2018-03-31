67ce3718de45d68f3457e1fe16b28f2b29914051
Victor Cabral <vccabral@gmail.com>
2018-03-31 02:05:17 -0400

    tweak

--- move_main_into_beat_wave.cpp
+++ move_main_into_beat_wave.cpp
@@ -10,6 +10,58 @@
 #define LED_TYPE WS2812                                       // Using APA102, WS2812, WS2801. Don't forget to change LEDS.addLeds.
 #define NUM_LEDS 175                                           // Number of LED's.
 uint8_t max_bright = 128;                                     // Overall brightness definition. It can be changed on the fly.
+//new stuff
+int _sample_size = 0; 
+bool auto_find_frequency_band = true;
+byte CHANGE_FREQUENCY_EVAL_CYCLES = 100;
+float STANDARD_DEV_WIDTH = 0.1;
+bool STD_DEV_INCREASE_MOVES_UP_SAMPLING_FREQUENCY = true;
+int SAMPLE_SIZE_TO_FREQUENCY_RATION = 60;
+int sample_hertz_ratio =  SAMPLE_SIZE_TO_FREQUENCY_RATION*8;
+double color_cycle_factor = pow(2.0, -10);
+double amplifier = 1.0 * pow(2.0, -6);
+int SIGNAL_MAX = 1;
+bool enable_sigmoid = false;
+double sigmoid_smooth_factor = 10; //higher means smoother
+bool enable_1_over_x = false;
+int red_base_line = 1;
+int green_base_line = 1;
+int blue_base_line = 1;
+int bluetooth_signal_pin = 5;
+int bluetooth_strength = 0;
+const uint16_t samples_0 = 8; 
+const uint16_t samples_1 = 16; 
+const uint16_t samples_2 = 32; 
+const uint16_t samples_3 = 64; 
+const uint16_t samples_4 = 128; 
+const uint16_t samples_5 = 256; 
+int count=0;
+double samplingFrequency = 0;
+unsigned int sampling_period_us;
+double last_std_dev = 0;
+double standard_deviation = 0;
+double vReal_0[samples_0];
+double vImag_0[samples_0];
+double pass_value_0[samples_0];
+double vReal_1[samples_1];
+double vImag_1[samples_1];
+double pass_value_1[samples_1];
+double vReal_2[samples_2];
+double vImag_2[samples_2];
+double pass_value_2[samples_2];
+double vReal_3[samples_3];
+double vImag_3[samples_3];
+double pass_value_3[samples_3];
+double vReal_4[samples_4];
+double vImag_4[samples_4];
+double pass_value_4[samples_4];
+double vReal_5[samples_5];
+double vImag_5[samples_5];
+double pass_value_5[samples_5];
+unsigned long microseconds;
+arduinoFFT FFT = arduinoFFT();
+int sum = 0;
+//new
 CRGB leds[NUM_LEDS];
 CRGB leds2[NUM_LEDS];
 CRGB leds3[NUM_LEDS];
